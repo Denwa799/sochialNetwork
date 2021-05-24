fb722be29db090a0b27bc28a8f6336337e91ba5d
Denwa <denwa799@yandex.ru>
2021-05-25 00:17:28 +0300

    Add useState hook to profile status

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styles from "./ProfileInfo.module.css";
 import Preloader from "../../common/Preloader/Preloader";
-import ProfileStatus from "./ProfileStatus";
+//import ProfileStatus from "./ProfileStatus";
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -20,7 +21,7 @@ const ProfileInfo = (props) => {
                     <div className={styles.descriptionText}>
                         <h1>{props.profile.fullName}</h1>
                     </div>
-                    <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+                    <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
                     <div className={styles.descriptionText}>
                         Обо мне: {props.profile.aboutMe ? props.profile.aboutMe : "Нет информации"}
                     </div>
